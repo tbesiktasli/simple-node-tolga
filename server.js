c8438b68f805617b275e0d121677cce6cc79ff59
tbesiktasli <tolga.besiktasli@gmail.com>
2020-12-11 19:27:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+const sagwas = process.env.SAGWAS
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
+    console.log('${sagwas}');
     console.log('Containers rule!');
     await sleep(5005);
   }
